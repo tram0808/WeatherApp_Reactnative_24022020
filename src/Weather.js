0a9ba87762ce0d51a795f7000f6d27a8770a3941
chiem3p <chiem2p@gmail.com>
2020-05-07 12:36:27 +0700

    update 2 API

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   FlatList,
   ActivityIndicator,
@@ -8,8 +8,8 @@ import {
   ScrollView,
   StyleSheet,
   TouchableOpacity,
-} from "react-native";
-import countryList from "../country";
+} from 'react-native';
+import countryList from '../country';
 export default class FetchExample extends React.Component {
   constructor(props) {
     super(props);
@@ -19,46 +19,46 @@ export default class FetchExample extends React.Component {
       city: {},
       country: countryList,
       data: countryList,
-      text: ""
+      text: '',
     };
   }
 
   setSearchText(event) {
     let searchText = event.nativeEvent.text;
-    this.setState({ text: searchText });
+    this.setState({text: searchText});
     let data = this.state.country;
     searchText = searchText.trim().toLowerCase();
-    data = data.filter(item => {
+    data = data.filter((item) => {
       return item.name.toLowerCase().match(searchText);
     });
     this.setState({
-      data: data
+      data: data,
     });
   }
 
-//   async componentDidMount() {
-//     this.setState({isLoading:true})
-//     try {
-//       const response = await fetch(
-//         `http://api.openweathermap.org/data/2.5/forecast?appid=b807789d6524ad85376b5961cc402be8&units=metric&id=${id}`
-//       );
-//       const responseJson = await response.json();
-//       alert(JSON.stringify(responseJson));
-//       if (responseJson) {
-//         this.setState({
-//           list: responseJson.list,
-//           city: responseJson.city,
-//           isLoading: false
-//         });
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       this.setState({ isLoading: false });
-//       alert("error");
-//     }
-//   }
-  onPressData = id => {
-    this.props.navigation.navigate("Detal", { id });
+  //   async componentDidMount() {
+  //     this.setState({isLoading:true})
+  //     try {
+  //       const response = await fetch(
+  //         `http://api.openweathermap.org/data/2.5/forecast?appid=b807789d6524ad85376b5961cc402be8&units=metric&id=${id}`
+  //       );
+  //       const responseJson = await response.json();
+  //       alert(JSON.stringify(responseJson));
+  //       if (responseJson) {
+  //         this.setState({
+  //           list: responseJson.list,
+  //           city: responseJson.city,
+  //           isLoading: false
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //       this.setState({ isLoading: false });
+  //       alert("error");
+  //     }
+  //   }
+  onPressData = (id) => {
+    this.props.navigation.navigate('Detal', {id});
     // this.setState({isLoading:true})
     // try {
     //   const response = await fetch(
@@ -85,7 +85,7 @@ export default class FetchExample extends React.Component {
     console.log(this.state.list);
     if (this.state.isLoading) {
       return (
-        <View style={{ flex: 1, padding: 20 }}>
+        <View style={{flex: 1, padding: 20}}>
           <ActivityIndicator size="large" color="red" />
         </View>
       );
@@ -104,32 +104,38 @@ export default class FetchExample extends React.Component {
         style={{
           flex: 1,
           padding: 20,
-          alignContent: "center",
-          alignItems: "center",
-          backgroundColor: "#ffbf00"
-        }}
-      >
-        <TextInput
-          placeholder="Please input ...."
-          style={{
-            width: 380,
-            height: 55,
-            fontSize: 19,
-            borderWidth: 1,
-            borderColor: "black",
-            paddingLeft: 10
-          }}
-          onChange={this.setSearchText.bind(this)}
-        />
-        <ScrollView style={{ marginTop: 12, width: 300 }}>
-          {this.state.data.map(item => {
-            return (
-              <TouchableOpacity onPress={() => this.onPressData(item.id)}>
-                <Text style={{ fontSize: 20 }}>{item.name}</Text>
-              </TouchableOpacity>
-            );
-          })}
-        </ScrollView>
+          alignContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#ffbf00',
+        }}>
+        <View style={{flex:0.1}}>
+          <TextInput
+            placeholder="Please input ...."
+            style={{
+              width: 380,
+              height: 55,
+              fontSize: 19,
+              borderWidth: 1,
+              borderColor: 'black',
+              paddingLeft: 10,
+            }}
+            onChange={this.setSearchText.bind(this)}
+          />
+        </View>
+        <View style={{flex:0.21}}>
+          <ScrollView style={{marginTop: 12, width: 300, height: 30}}>
+            {this.state.data.map((item) => {
+              return (
+                <TouchableOpacity onPress={() => this.onPressData(item.id)}>
+                  <Text style={{fontSize: 20}}>{item.name}</Text>
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
+        </View>
+        <View style={{flex:0.69}}>
+
+        </View>
       </View>
     );
   }
@@ -137,7 +143,7 @@ export default class FetchExample extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#ffbf00"
-  }
+    alignItems: 'center',
+    backgroundColor: '#ffbf00',
+  },
 });
--- src/Weather_detal.js
+++ src/Weather_detal.js
@@ -1,6 +1,14 @@
 //import liraries
-import React, { Component } from "react";
-import { View, Text, StyleSheet, ActivityIndicator, Image,ScrollView } from "react-native";
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  Image,
+  ScrollView,
+} from 'react-native';
+import {min} from 'react-native-reanimated';
 // import { ScrollView } from "react-native-gesture-handler";
 // import { Item } from "react-native/Libraries/Components/Picker/Picker";
 
@@ -8,56 +16,153 @@ import { View, Text, StyleSheet, ActivityIndicator, Image,ScrollView } from "rea
 class MyClass extends Component {
   constructor(props) {
     super(props);
-    this.state = { isLoading: true, city: {}, weather: [],hour:[] };
+    this.state = {isLoading: true, 
+      city: {},
+       weather: [],
+        // hour: [],
+        convdataTime:" ",
+      };
   }
 
   async componentDidMount() {
     const id = this.props.navigation.getParam('id');
     try {
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?appid=b807789d6524ad85376b5961cc402be8&units=metric&id=${id}`
+        `http://api.openweathermap.org/data/2.5/forecast?appid=b807789d6524ad85376b5961cc402be8&units=metric&id=${id}`,
       );
       const responseJson = await response.json();
-      alert(JSON.stringify(responseJson));
+      // alert(JSON.stringify(responseJson));
       if (responseJson) {
         this.setState({
           list: responseJson.list,
           city: responseJson.city,
-          isLoading: false
+          isLoading: false,
         });
+        // console.log(this.state.city);
       }
     } catch (error) {
       console.log(error);
-      this.setState({ isLoading: false });
-      alert("error");
+      this.setState({isLoading: false});
+      alert('error');
+    }
+
+    const lon = this.state.city.coord.lon;
+    const lat = this.state.city.coord.lat;
+    console.log(lon);
+    console.log(lat);
+    try {
+      const responseWeather = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3de6162d3745365b168ade2bbe4e1d66`,
+      );
+      const responseJsonWeather = await responseWeather.json();
+      // alert(JSON.stringify(responseJsonWeather));
+      if (responseJsonWeather) {
+        this.setState({
+          weather: responseJsonWeather.weather,
+          dt: responseJsonWeather.dt,
+          isLoading: false,
+        });
+        // console.log(this.state.city);
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({isLoading: false});
+      alert('error');
     }
   }
 
+  convert = () => {
+    // Unixtimestamp
+    let unixtimestamp = this.state.dt;
+console.log(unixtimestamp);
+    // Months array
+    let months_arr = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
 
-  
+    // Convert timestamp to milliseconds
+    let date = new Date(unixtimestamp * 1000);
+
+    // Year
+    let year = date.getFullYear();
+
+    // Month
+    let month = months_arr[date.getMonth()];
+
+    // Day
+    let day = date.getDate();
+
+    // Hours
+    let hours = date.getHours();
+
+    // Minutes
+    let minutes = '0' + date.getMinutes();
+
+    // Seconds
+    let seconds = '0' + date.getSeconds();
+    let convdataTim =
+    month +
+    '-' +
+    day +
+    '-' +
+    year +
+    ' ' +
+    hours +
+    ':' +
+    minutes.substr(-2) +
+    ':' +
+    seconds.substr(-2)
+    console.log("hello"+convdataTim)
+    // Display date time in MM-dd-yyyy h:m:s format
+     this.setState({convdataTime : convdataTim
+     });
+  };
+
   render() {
     if (this.state.isLoading) {
       return (
-        <View style={{ flex: 1, padding: 20 }}>
+        <View style={{flex: 1, padding: 20}}>
           <ActivityIndicator size="large" color="red" />
         </View>
       );
     }
-    return this.state.list.map(item => {
-      return (
-        <View><Text>{item.dt}</Text></View>
-      )
-    })
+    // return this.state.list.map(item => {
+    //   return (
+    //     <View><Text>{item.dt}</Text></View>
+    //   )
+    // })
     return (
       <View style={styles.Header}>
-        <Text style={{ fontSize: 30 }}>{this.state.city.name}</Text>
+        <Text style={{fontSize: 30}}>{this.state.city.name}</Text>
+        <Text>lat : {this.state.city.coord.lat}</Text>
+        <Text>long: {this.state.city.coord.lon}</Text>
+        <Text>{"hello"+this.state.convdataTime}</Text>
         <View>
-          <ScrollView style={{ width: 300 }}>
-            {this.state.list.map(item => {
-              return item.weather.map(it => {
+          <View>
+            {this.state.weather.map((i) => {
+              return <Text>Thoi tiet: {i.main}</Text>;
+            })}
+          </View>
+          <ScrollView style={{width: 300}}>
+            {this.state.list.map((item) => {
+              return item.weather.map((it) => {
                 return (
                   <View>
                     {/* <View>{this.timeStamp(item.dt, it.main)}</View> */}
+                    {/* <Text>{item.dt_txt}</Text>
+                    <Text>{item.dt_txt.split(' ')[0]}</Text>
+                    <Text>{it.main}</Text> */}
                     <View>
                       {/* <Text>{item.dt_txt.split(" ")[0]}</Text> */}
                       {/* {this.DateFunction(item.dt_txt)} */}
@@ -78,13 +183,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
 
-    backgroundColor: "#ffbf00"
+    backgroundColor: '#ffbf00',
   },
   Header: {
-    alignItems: "center",
+    alignItems: 'center',
     // justifyContent: "center",
-    backgroundColor: "#ffbf00"
-  }
+    backgroundColor: '#ffbf00',
+  },
 });
 
 //make this component available to the app
